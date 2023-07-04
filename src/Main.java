ee1c637a9b2d998052e7003c33dd9e649824825b
jp-avr <joaopedroazevedoreisveras@gmail.com>
2023-07-04 19:34:17 -0300

    criacao do projeto e da janela inicial

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -13,9 +13,13 @@ public class Main extends Application{
         try{
            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); //Instancear o objeto loader do tipo FXMLLoader
            //Serve para manipular a tela antes de carregar
-           Parent parent = loader.load();//Chamar o load e carrega a view
+           ScrollPane sp = loader.load();//Chamar o load e carrega a view
 
-           Scene mainScene = new Scene(parent);//Cena principal e passando como argumento o objeto principal da view
+           sp.setFitToHeight(true);
+           sp.setFitToWidth(true);     
+
+
+           Scene mainScene = new Scene(sp);//Cena principal e passando como argumento o objeto principal da view
            primaryStage.setScene(mainScene);//Setar a cena como a cena principal
            primaryStage.setTitle("ONE PIECE");//Titulo
            primaryStage.show();//Mostrar o palco
