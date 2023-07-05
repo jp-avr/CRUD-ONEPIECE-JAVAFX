bc453ae41fdb214562c69a52fe4b7ebcda986969
jp-avr <joaopedroazevedoreisveras@gmail.com>
2023-07-05 19:13:42 -0300

    Form do pirata e o registro

--- src/gui/PirataRegistrationController.java
+++ src/gui/PirataRegistrationController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Pirata;
 import model.services.PirataService;
@@ -45,8 +54,9 @@ public class PirataRegistrationController implements Initializable{
     private ObservableList<Pirata> obsList;
 
     @FXML
-    public void onBtNewAction() {
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent event) {
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("/gui/PirafaForm.fxml", parentStage);
     }
 
     public void setPirataService(PirataService service) {
@@ -77,6 +87,24 @@ public class PirataRegistrationController implements Initializable{
         List<Pirata> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewPirata.setItems(obsList);
+    }
+
+    //FUNÇÃO PARA CARREGAR OS DADOS DO FORMULÁRIO
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Preencha os dados");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL); //Enquanto não fechar não pode acessar a janela de trás
+            dialogStage.showAndWait();
+        }catch(IOException e) {
+            Alerts.showAlert("IO Exception", "ERROR loading view", e.getMessage(), AlertType.ERROR);
+        }
     }
     
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class Utils {
+    
+    public static Stage currentStage(ActionEvent event){
+        return (Stage) ((Node) event.getSource()).getScene().getWindow(); //Serve para acessar o Stage onde o controller que recebeu o evento está
+    }
+}
